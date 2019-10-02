ea96304ece6cd7f574ed15a15bca718ff3d35f57
DeePenguin <_deadpenguin_@mail.ru>
2019-10-03 00:22:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+function expressionCalculator(string) {
+  let expr = string.replace(/ /g, '').replace(/-/g, '~');
+
+  const divide = (x, y) => {
+    if (y === 0) throw TypeError("TypeError: Division by zero.");
+    else return x / y;
+  }
+
+  const parseDiv = (expr) => {
+    const parts = expr.split('/');
+    const partsToNums = parts.map(str => parseFloat(str)); 
+    return partsToNums.reduce((acc, num) => divide(acc, num));
+  };
+
+  const parseMult = (expr) => {
+    const parts = expr.split('*').map(part => parseDiv(part));
+    return parts.reduce((acc, num) => acc * num);
+  };
+
+  const parseMinus = (expr) => {
+    const parts = expr.split('~').map(part => parseMult(part));
+    return parts.reduce((acc, num) => acc - num);
+  };
+
+  const parsePlus = (expr) => {
+    const parts = expr.split('+').map(part => parseMinus(part));
+    return parts.reduce((acc, num) => acc + num);
+  };
+
+  const findPartInBrackets = (expr) => {
+    const openBracketInd = expr.lastIndexOf('(');
+    let closeBracketInd = expr.slice(openBracketInd).indexOf(')');
+    if (openBracketInd === -1 || closeBracketInd === -1) {
+      throw Error('ExpressionError: Brackets must be paired');
+    } else {
+      closeBracketInd += openBracketInd;
+      const currentPart = expr.slice(openBracketInd + 1, closeBracketInd);
+      return expr.slice(0, openBracketInd) + parsePlus(currentPart) + expr.slice(closeBracketInd + 1);
+    }
+  };
+
+  while (expr.indexOf('(') !== -1 || expr.indexOf(')') !== -1) expr = findPartInBrackets(expr);
+
+  return parsePlus(expr);
 }
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
